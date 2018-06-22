652cfda1eb9888192ffb08f9633044058887d9f6
Gabriel Gondim <ggondimrb@gmail.com>
2018-06-21 21:08:38 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gabrielgondim/cursomc/domain/Produto.java
+++ src/main/java/com/gabrielgondim/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -22,13 +24,17 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
-	
+	@JsonBackReference   // avisando que no outro relacionamento ja esta sendo chamado
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id") 
 		)
 	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
+		
+	}
 	
 	public Produto (Integer id, String nome, double preco) {
 		super();
