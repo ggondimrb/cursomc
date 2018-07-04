20dbca82e0c34d33e14175b12d644c215102d2ef
Gabriel Gondim <ggondimrb@gmail.com>
2018-07-04 20:40:10 -0300

    Endpoint /pedidos{id} disponibilizado

--- src/main/java/com/gabrielgondim/cursomc/domain/Cliente.java
+++ src/main/java/com/gabrielgondim/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.gabrielgondim.cursomc.domain.enums.TipoCliente;
 
@@ -39,6 +40,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 	
 	//lista nao se coloca no construtor
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();	
 	
