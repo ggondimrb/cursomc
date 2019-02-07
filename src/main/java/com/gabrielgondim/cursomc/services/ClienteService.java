8353147650526bd2533f690af30a03b34525c23d
Gabriel <ggondimrb@gmail.com>
2019-02-06 21:52:00 -0300

    PUT, DELETE E GET para CLiente

--- src/main/java/com/gabrielgondim/cursomc/services/ClienteService.java
+++ src/main/java/com/gabrielgondim/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.gabrielgondim.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gabrielgondim.cursomc.domain.Cliente;
+import com.gabrielgondim.cursomc.dto.ClienteDTO;
 import com.gabrielgondim.cursomc.repositories.ClienteRepository;
+import com.gabrielgondim.cursomc.services.exceptions.DataIntegrityException;
 import com.gabrielgondim.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +22,53 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 				
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir por que há entidades relacionadas");
+			// TODO: handle exception
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	//Page: lib do spring para realizar paginacao de busca dos itens do banco
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//metodo aux para validacao dos campos 
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setNome(obj.getNome());
+	}
+	
 }
