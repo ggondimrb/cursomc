991dab332f9551d00c981660a5855c4047249e08
Gabriel Gondim <ggondimrb@gmail.com>
2019-07-16 16:09:05 -0300

    email nao repetido na vavlidacao do Cliente

--- src/main/java/com/gabrielgondim/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/gabrielgondim/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,15 @@ package com.gabrielgondim.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabrielgondim.cursomc.domain.Categoria;
 import com.gabrielgondim.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/gabrielgondim/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/gabrielgondim/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gabrielgondim.cursomc.domain.Cliente;
 import com.gabrielgondim.cursomc.domain.enums.TipoCliente;
 import com.gabrielgondim.cursomc.dto.ClienteNewDTO;
+import com.gabrielgondim.cursomc.repositories.ClienteRepository;
 import com.gabrielgondim.cursomc.resources.exceptions.FieldMessage;
 import com.gabrielgondim.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email","E-mail já existente"));
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
