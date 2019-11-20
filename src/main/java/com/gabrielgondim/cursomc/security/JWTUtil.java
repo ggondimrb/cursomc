9121a3d278d07b7079412765d613b14b46341bb1
Gabriel Batista <ggondimrb@gmail.com>
2019-11-19 21:33:06 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/gabrielgondim/cursomc/security/JWTUtil.java
@@ -0,0 +1,29 @@
+package com.gabrielgondim.cursomc.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	// criando token com algoritimo HS512 muito poderoso e informando o tempo de expiracao da sessao
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+	}
+
+}
