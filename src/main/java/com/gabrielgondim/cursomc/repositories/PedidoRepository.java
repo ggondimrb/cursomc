cf436d0a43d73141d58821da2804a4b1aeed0af5
Gabriel Batista <ggondimrb@gmail.com>
2019-11-20 21:45:27 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/gabrielgondim/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/gabrielgondim/cursomc/repositories/PedidoRepository.java
@@ -1,12 +1,18 @@
 package com.gabrielgondim.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.gabrielgondim.cursomc.domain.Categoria;
+import com.gabrielgondim.cursomc.domain.Cliente;
 import com.gabrielgondim.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
