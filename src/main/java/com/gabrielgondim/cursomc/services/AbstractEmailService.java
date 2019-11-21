e20397c441f33e1d819c342d72f462667c1b5899
Gabriel Batista <ggondimrb@gmail.com>
2019-11-20 23:24:27 -0300

    esqueci a senha

--- src/main/java/com/gabrielgondim/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/gabrielgondim/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.gabrielgondim.cursomc.domain.Cliente;
 import com.gabrielgondim.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -69,5 +70,21 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(obj),true);
 						
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	private SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitaao de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
 	}
 }
